f0b8d46f7abf0d81c9629470f84da16a27427e49
root <root@Magnus.>
2025-01-24 23:22:48 +0800

    Level-0

---
+++ src/main/java/PawPal.java
@@ -0,0 +1,9 @@
+public class PawPal {
+    public static void main(String[] args) {
+        String name = "PawPal";
+        System.out.println("-----------------------------------------");
+        System.out.println("Hello! I'm " + name + "\n" + "What can I do for you? \n");
+        System.out.println("-----------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
