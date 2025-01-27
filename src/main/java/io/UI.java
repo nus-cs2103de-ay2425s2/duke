7e27536d2a3203164f24515c3976d6f1d97db16e
Andy Chong <79183296+andychong23@users.noreply.github.com>
2025-01-27 17:19:57 +0800

    Implement methods to display welcome and exit messages

--- src/main/java/io/UI.java
+++ src/main/java/io/UI.java
@@ -1,4 +1,38 @@
 package io;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * UI class to handle tasks related to the User Interface
+ */
 public class UI {
+    private final Input INPUT = new Input();
+    private final Output OUTPUT = new Output();
+    private final String DIVIDER = "_______________";
+    private final String CHATBOT_NAME;
+
+    /**
+     * Constructor
+     */
+    public UI(String botName) {
+        this.CHATBOT_NAME = botName;
+    }
+
+    public void displayWelcomeMessage() throws IOException {
+        List<String> welcomeMessages = new ArrayList<>();
+        welcomeMessages.add(this.DIVIDER);
+        welcomeMessages.add(String.format("Hello! I'm %s", this.CHATBOT_NAME));
+        welcomeMessages.add("What can I do for you?");
+        OUTPUT.printOutput(welcomeMessages, "\n", "\n");
+    }
+
+    public void displayExitMessage() throws IOException{
+        List<String> exitMessages = new ArrayList<>();
+        exitMessages.add(this.DIVIDER);
+        exitMessages.add("Bye. Hope to see you again soon!");
+        exitMessages.add(this.DIVIDER);
+        OUTPUT.printOutput(exitMessages, "\n", "\n");
+    }
 }
