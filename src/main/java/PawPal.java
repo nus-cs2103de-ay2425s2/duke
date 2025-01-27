5fd4b8279b7aacdddaeb873c2677f663fa880e27
friedrice231 <root@Magnus.>
2025-01-27 23:16:29 +0800

    Level 1

--- src/main/java/PawPal.java
+++ src/main/java/PawPal.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class PawPal {
     public static void main(String[] args) {
         String name = "PawPal";
         System.out.println("-----------------------------------------");
         System.out.println("Hello! I'm " + name + "\n" + "What can I do for you? \n");
         System.out.println("-----------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+
+            if (input.equalsIgnoreCase("bye")){
+                System.out.println("Bye. Hope to see you again soon! Meow");
+                break;
+            }
+            System.out.println("PawPal: " + input + " meow");
+        }
     }
 }
