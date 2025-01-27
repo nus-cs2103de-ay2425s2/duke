84b06972900dff4d8687e22a0c9f2543beb5532d
Andy Chong <79183296+andychong23@users.noreply.github.com>
2025-01-27 17:56:01 +0800

    Add ability for user to enter user input

--- src/main/java/bob/Bob.java
+++ src/main/java/bob/Bob.java
@@ -3,6 +3,8 @@ package bob;
 import io.UI;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bob {
     private final String CHATBOT_NAME = "Bob";
@@ -14,7 +16,22 @@ public class Bob {
     public static void main(String[] args) throws IOException {
         Bob bob = new Bob();
         UI ui = new UI(bob.getCHATBOT_NAME());
+
         ui.displayWelcomeMessage();
+        while (true) {
+            String userInput = ui.getUserInput();
+
+            // check if userInput bye
+            if (userInput.equals("bye")) {
+                break;
+            }
+
+            List<String> outputMessages = new ArrayList<>();
+            outputMessages.add(userInput);
+
+            ui.displayMessage(outputMessages);
+        }
+
         ui.displayExitMessage();
     }
 
