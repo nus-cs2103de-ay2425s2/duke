523d88e8580c71d80d102201c16681fe0d908ac1
Andy Chong <79183296+andychong23@users.noreply.github.com>
2025-01-27 16:35:53 +0800

    Add io package and the classes that are associated with it

---
+++ src/main/java/io/Input.java
@@ -0,0 +1,63 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Input class to handle the reading of all user inputs
+ */
+class Input {
+    BufferedReader bufferedReader;
+
+    /**
+     * Constructor
+     */
+    public Input() {
+        this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    /**
+     * Method to get user input
+     * @return Input string that user provides
+     * @throws IOException when IO fails
+     */
+    private String getUserInput() throws IOException {
+        return this.bufferedReader.readLine();
+    }
+
+    /**
+     * Checks if input is valid
+     * @param userInput Input string provided by the user
+     * @return boolean that indicates if the input is valid
+     */
+    private boolean isInputValid(String userInput) {
+        return true;
+    }
+
+    /**
+     * Gets valid user input
+     * @return Input String if the user input is valid
+     */
+    public String getValidUserInput() {
+        boolean isUserInputValid = false;
+
+        String userInput;
+
+        do {
+            try {
+                userInput = this.getUserInput();
+                if (this.isInputValid(userInput)) {
+                    isUserInputValid = true;
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        while (!isUserInputValid);
+
+        return userInput;
+
+    }
+}
