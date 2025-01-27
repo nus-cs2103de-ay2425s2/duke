5df03275a6c74a9304a4ea65e1ca58e5fc467b2c
Andy Chong <79183296+andychong23@users.noreply.github.com>
2025-01-27 17:20:11 +0800

    Rename name of chatbot to Bob

--- src/main/java/bob/Bob.java
+++ src/main/java/bob/Bob.java
@@ -1,12 +1,22 @@
-package duke;
+package bob;
 
-public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+import io.UI;
+
+import java.io.IOException;
+
+public class Bob {
+    private final String CHATBOT_NAME = "Bob";
+
+    public String getCHATBOT_NAME() {
+        return CHATBOT_NAME;
     }
+
+    public static void main(String[] args) throws IOException {
+        Bob bob = new Bob();
+        UI ui = new UI(bob.getCHATBOT_NAME());
+        ui.displayWelcomeMessage();
+        ui.displayExitMessage();
+    }
+
+
 }
