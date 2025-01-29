6ed6ac1c620f1761e272f3474c0edddd360c73a5
Andy Chong <andychong23@gmail.com>
2025-01-30 02:24:20 +0800

    Format string in getTaskList method of User class

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -20,7 +20,7 @@ public class User {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < taskList.size(); i++) {
             Task task = taskList.get(i);
-            stringBuilder.append("%s. %s %s".formatted(i + 1, task.getTaskName(), task.getTaskDetail()));
+            stringBuilder.append("%s. %s %s\n".formatted(i + 1, task.getTaskName(), task.getTaskDetail()));
         }
         return stringBuilder.toString();
     }
