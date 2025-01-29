b9ed2d90dacf467b61bd0bceafe01566674811e4
Andy Chong <andychong23@gmail.com>
2025-01-30 01:54:26 +0800

    Created abstract base class of Task

---
+++ src/main/java/event/Task.java
@@ -0,0 +1,25 @@
+package event;
+
+public abstract class Task {
+    protected TaskName taskName;
+
+    public enum TaskName {
+        READ,
+        RETURN,
+        ERROR
+    }
+
+    protected String getTaskName() {
+        return this.taskName.toString();
+    }
+
+    public static TaskName mapTaskName(String stringTaskName) {
+        return switch (stringTaskName) {
+            case "read" -> TaskName.READ;
+            case "return" -> TaskName.RETURN;
+            default -> TaskName.ERROR;
+        };
+    }
+
+    abstract String getTaskDetail();
+}
