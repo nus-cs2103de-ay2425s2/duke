29bb249054b6126659e05d8ae193669832bcd626
Andy Chong <andychong23@gmail.com>
2025-01-30 17:29:31 +0800

    Implemented isInputValid method
    Covers list,bye,mark,unmark for input validation

--- src/main/java/io/UI.java
+++ src/main/java/io/UI.java
@@ -1,5 +1,8 @@
 package io;
 
+import event.EventHandler;
+import event.EventHandler.Event;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -40,7 +43,34 @@ public class UI {
         return userInput;
     }
 
+    /**
+     * Check if user input is valid
+     * @param userInput Input string provided by the user with no trailing or leading whitespaces
+     * @return boolean to indicate if it is a valid userInput
+     */
     private boolean isInputValid(String userInput) {
+        String[] userInputTokens = userInput.split(" ");
+
+        if (userInputTokens[0].equalsIgnoreCase(Event.LIST.toString())
+                || userInputTokens[0].equalsIgnoreCase(Event.BYE.toString())) {
+            return userInputTokens.length == 1;
+        }
+        else if (userInputTokens[0].equalsIgnoreCase(Event.MARK.toString())
+                || (userInputTokens[0].equalsIgnoreCase(Event.UNMARK.toString()))) {
+            if (userInputTokens.length == 1) {
+                return false;
+            }
+            else if (userInputTokens.length > 2) {
+                return false;
+            }
+            else {
+                try {
+                    Integer.parseInt(userInputTokens[1]);
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
