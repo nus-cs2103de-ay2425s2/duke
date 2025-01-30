0d546007e2fd5dfec508b2a435081ef02ecbefa2
Andy Chong <andychong23@gmail.com>
2025-01-30 22:12:15 +0800

    Implement the code for EventTask class

--- src/main/java/task/EventTask.java
+++ src/main/java/task/EventTask.java
@@ -2,8 +2,31 @@ package task;
 
 import action.ActionHandler.Action;
 
-public class EventTask extends Task {
-    public EventTask(String taskDetail) {
+public class EventTask extends Task implements HasStart, HasDeadline {
+    private String fromDateTime;
+    private String toDateTime;
+
+    public EventTask(String taskDetail, String fromDateTime, String toDateTime) {
         super(taskDetail, Action.EVENT);
+        this.fromDateTime = fromDateTime;
+        this.toDateTime = toDateTime;
+    }
+
+    @Override
+    public String getDeadLine() {
+        return this.getToDateTime();
+    }
+
+    @Override
+    public String getStartDateTime() {
+        return this.getToDateTime();
+    }
+
+    private String getFromDateTime() {
+        return this.fromDateTime;
+    }
+
+    private String getToDateTime() {
+        return this.toDateTime;
     }
 }
