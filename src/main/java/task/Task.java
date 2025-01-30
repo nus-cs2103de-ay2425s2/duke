bdf4e5486ba74d1062efee086bb226e4a643cd15
Andy Chong <andychong23@gmail.com>
2025-01-30 16:55:11 +0800

    Implemented attribute to check if Task is done
    Implemented getters and setters to access the attribute

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -2,12 +2,22 @@ package task;
 
 public class Task {
     private String taskDetail;
+    private boolean isTaskDone;
 
     public Task(String taskDetail) {
         this.taskDetail = taskDetail;
+        this.isTaskDone = false;
     }
 
     public String getTaskDetail() {
         return this.taskDetail;
     };
+
+    public boolean isTaskDone() {
+        return this.isTaskDone;
+    }
+
+    public void setTaskDone(boolean taskDone) {
+        this.isTaskDone = taskDone;
+    }
 }
