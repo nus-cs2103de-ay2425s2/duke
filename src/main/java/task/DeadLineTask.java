4a48d431eded5242953cc19976f9923bb6bf593b
Andy Chong <andychong23@gmail.com>
2025-01-30 22:11:35 +0800

    Implement the code for DeadLineTask class

--- src/main/java/task/DeadLineTask.java
+++ src/main/java/task/DeadLineTask.java
@@ -2,8 +2,16 @@ package task;
 
 import action.ActionHandler;
 
-public class DeadLineTask extends Task {
-    public DeadLineTask(String taskDetail) {
+public class DeadLineTask extends Task implements HasDeadline {
+    private String deadLine;
+
+    public DeadLineTask(String taskDetail, String deadLine) {
         super(taskDetail, ActionHandler.Action.DEADLINE);
+        this.deadLine = deadLine;
+    }
+
+    @Override
+    public String getDeadLine() {
+        return this.deadLine;
     }
 }
