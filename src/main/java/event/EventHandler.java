c8da4d04f7d6780877a86c04e32898fd22239414
Andy Chong <andychong23@gmail.com>
2025-01-30 16:35:14 +0800

    Implemented processEvent method code
    Add header comments to class

--- src/main/java/event/EventHandler.java
+++ src/main/java/event/EventHandler.java
@@ -2,15 +2,65 @@ package event;
 
 import event.Task.TaskName;
 
-import java.util.HashMap;
-import java.util.Map;
+import user.User;
 
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Class to handle events that should be happen
+ */
 public class EventHandler {
-    // to figure out a way to process an event
-    public void processEvent(String eventString) {
 
+    /**
+     * Method to process events that are input to the user
+     * Precondition: eventString is already a valid input by the user
+     * @param eventString Input String provided by the user
+     * @param user User class that indicates the current user
+     * @return List of Strings that indicate what needs to be output to the console
+     */
+    public List<String> processEvent(String eventString, User user) {
+        List<String> outputMessages = new ArrayList<>();
+
+        String[] eventStringTokens = eventString.split(" ");
+
+        if (eventStringTokens[0].equalsIgnoreCase("list")) {
+            outputMessages.add("Here are the tasks in your list: ");
+            outputMessages.add(user.getTaskList());
+        }
+        else if (eventStringTokens[0].equalsIgnoreCase("bye")) {
+            return outputMessages;
+        }
+        else {
+            String stringTaskName = eventStringTokens[0];
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 1; i < eventStringTokens.length; i++) {
+                stringBuilder.append(eventStringTokens[i]);
+
+                if (i != eventStringTokens.length - 1) {
+                    stringBuilder.append(" ");
+                }
+            }
+
+            String taskDetail = stringBuilder.toString();
+
+            Task task = createTask(eventStringTokens[0], taskDetail);
+
+            user.addTask(task);
+
+            outputMessages.add("added: %s %s".formatted(stringTaskName, taskDetail));
+        }
+
+        return outputMessages;
     }
 
+    /**
+     * Create Task
+     * @param stringTaskName String representation of TaskName enum
+     * @param taskDetail String representation for the details of the task
+     * @return Task created
+     */
     private Task createTask(String stringTaskName, String taskDetail) {
         TaskName taskName = Task.mapTaskName(stringTaskName);
 
