5306318611754a3a26231ac68bfedf170b8d9e7e
Andy Chong <andychong23@gmail.com>
2025-01-30 23:05:09 +0800

    Set up automated testing

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -50,7 +50,7 @@ fi
 
 # compile the code into the bin folder, terminates if error occurred
 echo -e "\n${BOLD}[3/4] Compiling source files...${NC}"
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*/*.java
 then
     echo -e "\n${RED}╔════ ERROR ══════════════════════════════════╗${NC}"
     echo -e "${RED}║            BUILD FAILURE                     ║${NC}"
@@ -61,7 +61,7 @@ echo -e "${GREEN}✓ Compilation successful${NC}\n"
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 echo -e "${BOLD}[4/4] Running tests...${NC}"
-java -classpath ../bin Bob < input.txt > ACTUAL.TXT
+java -classpath ../bin bob.Bob < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
