bce9703b13e573c6d3d1ee25e325fb7ee960e4ff
Andy Chong <andychong23@gmail.com>
2025-01-30 17:31:30 +0800

    Add markTaskAsDone, markTaskAsNotDone, getMarkTaskConsoleOutput and modified getTaskList
    markTaskAsDone and markTaskAsNotDone changes the status of the Task number indicated.
    
    getMarkTaskConsoleOutput returns the output message required to be displayed to the user
    
    getTaskList is changed to include the task status

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -20,11 +20,37 @@ public class User {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < taskList.size(); i++) {
             Task task = taskList.get(i);
-            stringBuilder.append("%s. %s".formatted(i + 1, task.getTaskDetail()));
+            stringBuilder.append("%s.[%s] %s".formatted(
+                    i + 1,
+                    (task.isTaskDone()) ? "X" : " ",
+                    task.getTaskDetail())
+            );
             if (i != taskList.size() - 1) {
                 stringBuilder.append("\n");
             }
         }
         return stringBuilder.toString();
     }
+
+    public String markTaskAsDone(int taskNumber) {
+        Task requiredTask = this.taskList.get(taskNumber);
+        requiredTask.setTaskDone(true);
+
+        return this.getMarkTaskConsoleOutput(requiredTask.isTaskDone(), requiredTask.getTaskDetail());
+    }
+
+    public String markTaskAsNotDone(int taskNumber) {
+        Task requiredTask = this.taskList.get(taskNumber);
+        requiredTask.setTaskDone(false);
+
+        return this.getMarkTaskConsoleOutput(requiredTask.isTaskDone(), requiredTask.getTaskDetail());
+    }
+
+    private String getMarkTaskConsoleOutput(boolean isTaskDone, String taskDetail) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[%s] %s".formatted((isTaskDone) ? "X" : " ", taskDetail));
+        System.out.println("test" + stringBuilder.toString());
+        return stringBuilder.toString();
+    }
+
 }
