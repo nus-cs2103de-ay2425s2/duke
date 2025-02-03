a8165dd6deb0bb6e961a57582c7b3a53654ff7ea
Andy Chong <79183296+andychong23@users.noreply.github.com>
2025-02-03 17:09:16 +0800

    Add initial implementation of DataHandler
    Methods such as writeFile and readFile is implemented

--- src/main/java/data/DataHandler.java
+++ src/main/java/data/DataHandler.java
@@ -1,14 +1,33 @@
 package data;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class DataHandler {
-    public static void writeFile(Path filePath, List<String> payLoad, boolean reWrite){
+    public static void writeFile(Path filePath, List<String> payLoad, boolean isAppend) throws IOException {
+        // file does not exist
+        if (!Files.exists(filePath)) {
+            Files.writeString(filePath, String.join("\n", payLoad), StandardOpenOption.CREATE_NEW);
+            return;
+        }
 
+        //file exists
+        if (isAppend) {
+            Files.writeString(filePath, String.join("\n", payLoad), StandardOpenOption.APPEND);
+        }
+        else {
+            Files.writeString(filePath, String.join("\n", payLoad), StandardOpenOption.TRUNCATE_EXISTING);
+        }
     };
 
-    public static List<String> readFile(Path filePath) {
-        return null;
+    public static List<String> readFile(Path filePath) throws IOException {
+        if (!Files.exists(filePath)) {
+            throw new FileNotFoundException("File does not exists");
+        }
+        return Files.readAllLines(filePath);
     }
 }
