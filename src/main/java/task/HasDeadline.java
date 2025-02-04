e40288413968ff3368e8896ab8c9321b25d1a55a
Andy Chong <andychong23@gmail.com>
2025-02-05 00:24:13 +0800

    Add DateTimeFormatters that can be used by objects implementing the interfaces

--- src/main/java/task/HasDeadline.java
+++ src/main/java/task/HasDeadline.java
@@ -1,8 +1,12 @@
 package task;
 
+import java.time.format.DateTimeFormatter;
+
 /**
  * HasDeadline Interface
  */
 public interface HasDeadline {
+    static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd MMM yyyy");
+    static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd MMM yyyy hh:mma");
     public String getDeadLine();
 }
